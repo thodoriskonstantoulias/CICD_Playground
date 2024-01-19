2532a3f282f747a7630c2ee58ed1ebde08d21e40
Theodoros Konstantoulias <thod.konstantoulias@hotmail.com>
2024-01-19 22:58:59 +0200

    test

--- CICD.Api.1/Program.cs
+++ CICD.Api.1/Program.cs
@@ -23,12 +23,12 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index => 
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)] + "Test deploy AZURE"
+            summaries[Random.Shared.Next(summaries.Length)] + "Test deploy AZURE github again"
         ))
         .ToArray();
     return forecast;
