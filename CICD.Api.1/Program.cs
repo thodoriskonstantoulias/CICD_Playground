9cd8d3dd9a5a62653a48da8841c7461f89dbf707
Theodoros Konstantoulias <thod.konstantoulias@hotmail.com>
2024-01-20 19:27:32 +0200

    yaml

--- CICD.Api.1/Program.cs
+++ CICD.Api.1/Program.cs
@@ -28,7 +28,7 @@ app.MapGet("/weatherforecast", () =>
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)] + " Test github again 7"
+            summaries[Random.Shared.Next(summaries.Length)] + " Test yaml"
         ))
         .ToArray();
     return forecast;
